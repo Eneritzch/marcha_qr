ce492e8bf068be9a142530c18ec76bb7dfbf1b16
Eneritzch <jaionecherres4@gmail.com>
2026-01-27 18:18:02 -0500

    deplooooy

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Storage for WhiteNoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Storage for WhiteNoise (Compressed but NOT hashed to avoid breaking ES6 modules)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
