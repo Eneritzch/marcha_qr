2e793bc16d5f76c51eba44557bcd11fc009b9d1e
Eneritzch <jaionecherres4@gmail.com>
2026-01-27 19:08:50 -0500

    Prego

--- core/utils.py
+++ core/utils.py
@@ -76,7 +76,7 @@ class QRGenerator:
         img_qr = img
         
         # 3. Embed Logo
-        logo_path = os.path.join(settings.MEDIA_ROOT, 'icono.png')
+        logo_path = os.path.join(settings.BASE_DIR, 'static', 'img', 'icono.png')
         
         if os.path.exists(logo_path):
             try:
