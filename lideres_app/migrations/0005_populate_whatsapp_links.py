d92490c7d225615a148551218820701c80ff334e
sveram <123789007+RonnyAreUneMi@users.noreply.github.com>
2026-01-28 19:52:38 -0500

    whatsapp actualizacion

--- lideres_app/migrations/0005_populate_whatsapp_links.py
+++ lideres_app/migrations/0005_populate_whatsapp_links.py
@@ -4,21 +4,21 @@ def populate_whatsapp_links(apps, schema_editor):
     GrupoConfig = apps.get_model('lideres_app', 'GrupoConfig')
     
     links = {
-        1: "https://chat.whatsapp.com/GB2eq17U275CYl2KFkxrvH?mode=gi_t",
-        2: "https://chat.whatsapp.com/IN5SRnaE7mC5XLtefVyDnq?mode=gi_t",
-        3: "https://chat.whatsapp.com/Ku9fEG1IYyrHvhqhMyXuqM?mode=gi_t",
-        4: "https://chat.whatsapp.com/HF20TpiGWAB4jQNW9lrDcy?mode=gi_t",
-        5: "https://chat.whatsapp.com/Jg4hqvQ5XBhCekP9sRjhcv?mode=gi_t",
+        1: "https://chat.whatsapp.com/HbYYngvGGCF9hxpTCOrLNk?mode=gi_t",
+        2: "https://chat.whatsapp.com/IN5SRnaE7mC5XLtefVyDnq",
+        3: "https://chat.whatsapp.com/H14k1Ss5p4o9NVQJZzDIcC",
+        4: "https://chat.whatsapp.com/HF20TpiGWAB4jQNW9lrDcy",
+        5: "https://chat.whatsapp.com/FGvYNGzHOMoLnG9adQQogU",
         6: "https://chat.whatsapp.com/Cz6PDOssA3N2N35qCIGBaW?mode=gi_t",
-        7: "https://chat.whatsapp.com/K1ofEW3cCrO2p2mrq3WSYF?mode=gi_t",
+        7: "https://chat.whatsapp.com/LRZ8PFF65unDRgAcvjrbGL",
         8: "https://chat.whatsapp.com/Bnhxa08c7oMB4yBMiReChp?mode=gi_t",
-        9: "https://chat.whatsapp.com/GmZEA5HAjiH4xkTVPdEWtk?mode=gi_t",
-        10: "https://chat.whatsapp.com/GKpA6veGvLTCnqfAU3I066?mode=gi_t",
-        11: "https://chat.whatsapp.com/HbYYngvGGCF9hxpTCOrLNk?mode=gi_t",
-        12: "https://chat.whatsapp.com/Ga5wW4KmDJgA3PAWPtbHJx?mode=gi_t",
-        13: "https://chat.whatsapp.com/FVLzra0wbju9Vj0oEsZlNw?mode=gi_t",
-        14: "https://chat.whatsapp.com/LakAcpvtO681Jlf1dQypLn?mode=gi_t",
-        15: "https://chat.whatsapp.com/K5fzxG0IV7S4UqZl3XqmXS?mode=gi_t",
+        9: "https://chat.whatsapp.com/GmZEA5HAjiH4xkTVPdEWtk",
+        10: "https://chat.whatsapp.com/GKpA6veGvLTCnqfAU3I066",
+        11: "https://chat.whatsapp.com/BQbK2FbUsNAAvE0cq7c4AU",
+        12: "https://chat.whatsapp.com/JLpqR4UAuig6Tirkr6oL6r",
+        13: "https://chat.whatsapp.com/FVLzra0wbju9Vj0oEsZlNw",
+        14: "https://chat.whatsapp.com/JmlGSJD2xGb1Sn45GOj0uX",
+        15: "https://chat.whatsapp.com/EiSZvJ9eozn9nxPTg1LlFM",
     }
     
     for numero, link in links.items():
